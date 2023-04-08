f15cf0685a9d7742ee765f93f8ae2dd1a6fd14cb
Deveroonie <sirepiclydev@gmail.com>
2023-04-08 20:23:09 +0100

    get page

---
+++ assets/js/getPage.js
@@ -0,0 +1,33 @@
+function getPlatFromLong(text) {
+    let words = text.split(" ");
+    
+    if (words.length > 1) {
+      words = [words.pop()];
+    } else {
+      words = [];
+    }
+    return words
+    }
+    function doUpdate(num, r) {
+        const d = new Date(r[num].expectedArrival).toLocaleTimeString('en-GB', {timeZone: 'GMT'})
+        document.getElementById(`eta${num + 1}`).innerText = d
+        document.getElementById(`dest${num + 1}`).innerText = r[num].destinationName
+        document.getElementById(`plat${num + 1}`).innerText = getPlatFromLong(r[num].platformName)
+        document.getElementById(`line${num + 1}`).innerText = r[num].lineName
+    }
+    window.addEventListener("load", () => {
+        console.log("gtf")
+    
+        axios.get(`https://api.tfl.gov.uk/StopPoint/940GZZLUOXC/Arrivals?cacheBypass=${Math.random()*100000000}`).then(function(res) {
+            console.log("gtf")
+    
+        const r = res.data
+        doUpdate(0, r)
+        doUpdate(1, r)
+        doUpdate(2, r)
+        doUpdate(3, r)
+        doUpdate(4, r)
+        
+    })
+    })
+    
