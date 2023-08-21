ae88956f60e113d696b68a8b12cd5e825145e598
Deveroonie <sirepiclydev@gmail.com>
2023-08-21 11:39:33 +0100

    use foreach + fix up UI design.

--- assets/js/getPage.js
+++ assets/js/getPage.js
@@ -8,10 +8,16 @@ function getPlatFromLong(text) {
     }
     return words
     }
-    function doUpdate(num, r) {
-        const dR = new Date(r[num].expectedArrival)
-        dR.setHours(dR.getHours() + 1)
-        const d = dR.toLocaleTimeString('en-GB', {timeZone: 'GMT'})
+    function doUpdate(r) {
+        
+        const table = document.getElementById("table")
+        const tr = document.createElement("tr")
+
+
+
+
+
+
         document.getElementById(`eta${num + 1}`).innerText = d
         document.getElementById(`dest${num + 1}`).innerText = r[num].destinationName
         document.getElementById(`plat${num + 1}`).innerText = getPlatFromLong(r[num].platformName)
@@ -27,11 +33,39 @@ function getPlatFromLong(text) {
             console.log("gtf")
     
         const r = res.data
-        doUpdate(0, r)
-        doUpdate(1, r)
-        doUpdate(2, r)
-        doUpdate(3, r)
-        doUpdate(4, r)
+        console.log(r)
+
+        r.forEach(function callback(e, i) { //e -> array item, i -> array index
+          
+          const table = document.getElementById("table")
+          const tr = document.createElement("tr")
+
+
+          const dR = new Date(e.expectedArrival)
+          dR.setHours(dR.getHours() + 1)
+          const d = dR.toLocaleTimeString('en-GB', {timeZone: 'GMT'})
+          const eta = document.createElement("td")
+          eta.id = `eta${i+1}`
+          eta.innerText = d
+          tr.appendChild(eta)
+
+          const dest = document.createElement("td")
+          dest.id = `dest${i+1}`
+          dest.innerText = e.destinationName;
+          tr.appendChild(dest)
+
+          const plat = document.createElement("td")
+          plat.id = `plat${i+1}`
+          plat.innerText = e.platformName
+          tr.appendChild(plat)
+
+          const line = document.createElement("td")
+          line.id = `line${i+1}`
+          line.innerText = e.lineName
+          tr.appendChild(line)
+
+          table.appendChild(tr)
+        })
         
     })
     })
